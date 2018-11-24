b2d498339f265f65e27ff2160e1d7cef6ca95c64
Chris Muthig <camuthig@gmail.com>
2018-11-24 13:51:11 -0800

    Add links to the landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,19 @@
 <div class="hero is-fullheight-with-navbar is-black">
     <div class="hero-body">
         <div class="container">
-            <div class="columns is-vcentered">
-                <div class="column is-offset-one-fifth-desktop is-two-thirds-desktop">
+            <div class="columns is-centered is-vcentered">
+                <div class="column is-two-thirds-tablet">
                     <h1 class="title">
                         Vinopedia
                     </h1>
                     <h2 class="subtitle">
                         An almanac for viticultural knowledge maintained by wine professionals
                     </h2>
+                    <div class="buttons is-centered">
+                        <a href="{{ route('varieties.index') }}" class="button is-black is-inverted is-outlined subtitle">Varieties</a>
+                        <a href="{{ route('home') }}" class="button is-black is-inverted is-outlined subtitle">Regions</a>
+                    </div>
+
                 </div>
             </div>
         </div>
